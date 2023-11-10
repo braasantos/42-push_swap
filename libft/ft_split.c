1ef7b02c1d182db1397a7dfc1293aecf5f8c8c1f
Braulio Jorge Monteiro Dos Santos <bjorge-m@c1r14s12.42porto.com>
2023-11-10 16:49:51 +0000

    aaa

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -5,73 +5,117 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bjorge-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/27 12:08:41 by bjorge-m          #+#    #+#             */
-/*   Updated: 2023/10/27 12:31:24 by bjorge-m         ###   ########.fr       */
+/*   Created: 2023/10/12 15:08:10 by bjorge-m          #+#    #+#             */
+/*   Updated: 2023/10/12 15:16:12 by bjorge-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_countwords(char const *str, char c)
+static int	ft_countword(char const *s, char c)
 {
-	int	i;
 	int	flag;
 	int	count;
 
-	i = 0;
-	flag = 1;
+	flag = 0;
 	count = 0;
-	while (str[i])
+	while (*s)
 	{
-		if (str[i] == c)
+		if (*s == c)
 			flag = 0;
-		else 
+		else
 		{
-			if (flag == 1)
+			if (!flag)
 			{
 				count++;
-				flag = 0;
+				flag = 1;
 			}
 		}
-		i++;
+		s++;
 	}
 	return (count);
 }
 
-char	**ft_getarr(char const *s, char **str, char c)
+int	ft_free(char **new_arr, int count)
 {
 	int	i;
-	int	j;
-	int	k;
-	char	**newarr;
-	
-	newarr = str;
-	if (!newarr)
-		return (NULL);
+	int	flag;
+
 	i = 0;
-	j = 0;
-	k = 0;
-	while (s[i])
+	flag = 0;
+	while (i < count)
+		if (!new_arr[i++])
+			flag = 1;
+	if (flag)
 	{
-		if (s[i] != c)
-		{
-			newarr = (char **)ft_calloc(*s + 1, sizeof(char));
-			newarr[j][k] = s[i];
-			k++;
-			i++;
-		}
-		newarr[j][k] = '\0';
-		j++;
-		i++;
+		i = 0;
+		while (i < count)
+			free(new_arr[i++]);
+		free(new_arr);
+		return (0);
 	}
-	newarr[j] = NULL;
+	return (1);
+}
+
+static char	**ft_putwords(char **new, char const *s, char c)
+{
+	char	**newarr;
+	int		flag;
+	int		count;
+
+	newarr = new; 
+	flag = 0;
+	count = 0;
+	while (*s)
+	{
+		if (*s == c)
+			flag = 0;
+		else
+		{
+			if (!flag)
+			{
+				newarr[count] = ft_substr(s, 0, ft_strchr(s, c) - s);
+				flag = 1;
+				count++;
+			}
+		}
+		s++;
+	}
+	newarr[count] = NULL;
 	return (newarr);
 }
 
-char	**ft_split(char const *str, char c)
+char	**ft_split(char const *s, char c)
 {
-	char	**arr;
-	arr =(char **)ft_calloc(ft_countwords(str, c) + 1, sizeof(char *));
-	ft_getarr(str, arr, c);
-	return (arr);
+	char	**new_arr;
+	int		count;
+
+	if (!s)
+		return (NULL);
+	count = ft_countword(s, c);
+	new_arr = (char **)ft_calloc((count + 1), sizeof(char *));
+	if (!new_arr)
+		return (NULL);
+	ft_putwords(new_arr, s, c);
+	if (!ft_free(new_arr, count))
+		return (NULL);
+	return (new_arr);
 }
+/*int main(void)
+{
+    char const *s = "isso e um teste para ver se a split funciona";
+    char c = ' ';
+    
+    char **result = ft_split(s, c);
+ if (result) {
+        for (int i = 0; result[i] != NULL; i++) {
+            printf("array : %s\n", result[i]);
+            free(result[i]); // Liberar a memória alocada para cada palavra
+        }
+        free(result); // Liberar a memória alocada para o array de strings
+    } else {
+        // Trate a falha na alocação de memória aqui
+    }
+
+    return 0;
+}*/
---
+++ push_swap.h
@@ -0,0 +1,14 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+
+// List struct
+
+typedef struct push_swap
+{
+  /* data */
+};
+
+
+
+#endif
